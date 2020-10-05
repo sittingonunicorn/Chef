a9841636c0aead5f6e08d16de5d5aad7384e01cf
Lina Che <lina_chentsova@epam.com>
2020-10-05 15:59:22 +0300

    Edit quantity

--- src/main/java/model/service/Menu.java
+++ src/main/java/model/service/Menu.java
@@ -20,7 +20,7 @@ public class Menu {
                 new Vegetable(Ingredients.WHITE_CABBAGE, 50, false), new Vegetable(Ingredients.CARROT, 30, false),
                 new Vegetable(Ingredients.GARLIC, 5, false), new Ingredient(Ingredients.SALT, 5),
                 new Ingredient(Ingredients.PARSLEY, 5), new Ingredient(Ingredients.PEPPER, 5),
-                new Ingredient(Ingredients.VINEGAR, 10));
+                new Ingredient(Ingredients.VINEGAR, 20));
         VINAIGRETTE = new Salad("salad.vinaigrette", new Vegetable(Ingredients.POTATOES, 25, true),
                 new Vegetable(Ingredients.BEET, 50, true), new Vegetable(Ingredients.CARROT, 25, true),
                 new Vegetable(Ingredients.GREEN_PEAS, 30, false), new Vegetable(Ingredients.CUCUMBERS, 30, false),
